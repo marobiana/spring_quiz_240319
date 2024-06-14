3579b958b57e759d6c0f0fc622f2cea89fcaa2e1
DESKTOP-A4BK9EQ\user <test@test.com>
2024-06-14 18:48:23 +0900

    3강 문제1-2 일부까지

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,29 @@
+package com.quiz.lesson03.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.domain.RealEstate;
+import com.quiz.lesson03.mapper.RealEstateMapper;
+
+@Service
+public class RealEstateBO {
+	
+	@Autowired
+	private RealEstateMapper realEstateMapper;
+
+	// input: id(int)
+	// output: RealEstate(단건) or null
+	public RealEstate getRealEstateById(int id) {
+		return realEstateMapper.selectRealEstateById(id);
+	}
+	
+	// input: rentPrice(필수)
+	// output: List<RealEstate>
+	public RealEstate getRealEstateListByRentPrice(int rentPrice) {
+		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice);
+	}
+}
+
+
+
