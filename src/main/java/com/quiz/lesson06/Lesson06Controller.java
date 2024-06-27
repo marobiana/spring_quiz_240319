6f86d29997762163f35a0ee3aaed62f0b6eff1af
DESKTOP-A4BK9EQ\user <test@test.com>
2024-06-27 16:51:13 +0900

    6강 문제1 풀이 일부

--- src/main/java/com/quiz/lesson06/Lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Controller.java
@@ -1,12 +1,22 @@
 package com.quiz.lesson06;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
 
 @RequestMapping("/lesson06")
 @Controller
 public class Lesson06Controller {
+	
+	@Autowired
+	private BookmarkBO bookmarkBO;
 
 	// 즐겨찾기 추가 화면
 	// http://localhost:8080/lesson06/add-bookmark-view
@@ -14,11 +24,26 @@ public class Lesson06Controller {
 	public String addBookmarkView() {
 		return "lesson06/addBookmark";
 	}
+	
+	// AJAX가 하는 요청
+	// 즐겨찾기 추가 로직
+	@ResponseBody
+	@PostMapping("/add-bookmark")
 
 	// 즐겨찾기 목록 화면
 	// http://localhost:8080/lesson06/bookmark-list-view
 	@GetMapping("/bookmark-list-view")
-	public String bookmarkListView() {
+	public String bookmarkListView(Model model) {
+		// db select => List<Bookmark>
+		List<Bookmark> bookmarkList = bookmarkBO.getBookmarkList();
+		
+		// model 담기
+		model.addAttribute("bookmarkList", bookmarkList);
+		
 		return "lesson06/bookmarkList";
 	}
 }
+
+
+
+
