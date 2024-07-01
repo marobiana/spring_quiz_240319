d21bea23c69f9b6e225c7b3542e479850537fad0
DESKTOP-A4BK9EQ\user <test@test.com>
2024-07-01 17:46:36 +0900

    통나무펜션 조회/삭제/추가 풀이

---
+++ src/main/java/com/quiz/booking/bo/BookingBO.java
@@ -0,0 +1,40 @@
+package com.quiz.booking.bo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.booking.domain.Booking;
+import com.quiz.booking.mapper.BookingMapper;
+
+@Service
+public class BookingBO {
+	
+	@Autowired
+	private BookingMapper bookingMapper;
+
+	// input: X
+	// output: List<Booking>
+	public List<Booking> getBookingList() {
+		return bookingMapper.selectBookingList();
+	}
+	
+	// input:id
+	// output:int
+	public int deleteBookingById(int id) {
+		return bookingMapper.deleteBookingById(id);
+	}
+	
+	public void addBooking(String name, LocalDate date,
+			int day, int headcount, String phoneNumber) {
+		
+		bookingMapper.insertBooking(name, date, day, headcount, phoneNumber);
+	}
+}
+
+
+
+
---
+++ src/main/java/com/quiz/booking/mapper/BookingMapper.java
@@ -0,0 +1,30 @@
+package com.quiz.booking.mapper;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.quiz.booking.domain.Booking;
+
+@Mapper
+public interface BookingMapper {
+
+	public List<Booking> selectBookingList();
+	
+	public int deleteBookingById(int id);
+	
+	public void insertBooking(
+			@Param("name") String name, 
+			@Param("date") LocalDate date,
+			@Param("day") int day, 
+			@Param("headcount") int headcount, 
+			@Param("phoneNumber") String phoneNumber);
+}
+
+
+
+
+
+
